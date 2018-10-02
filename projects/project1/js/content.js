62692057ffb14514f7946a5363829b436e834f43
zean7760 <zean7760@colrado.edu>
2018-10-02 10:25:37 -0600

    added avg salary to p1, and p1 to portal

--- projects/project1/js/content.js
+++ projects/project1/js/content.js
@@ -1,11 +1,9 @@
-
-
 $( ".cta" ).click(function() {
     $( ".transition").toggleClass( "anim-trans" );
     $("#headerImg").css('width','40%');
     $("#headerImg").css('margin','0px auto 0px auto');
     $("#headerImg").hide( 2500 ).show( 2000 );
-    $("#mapPop").show( 2500 ).fadeIn( 2500 );
+    $("#mapPop").show( 4000 ).fadeIn( 4000 );
     $("#why").css('display', 'none');
     $("#blocks").css('display', 'none');
   });
@@ -34,21 +32,18 @@ $("path, circle").hover(function(e) {
   }
 
   $(document).ready(function(){
-    $("#headerImg").load(function(){
-        $("#headerImg").fadeOut( 700 );
-    });
+
     var click = 0;
     var click2 = 0;
 
 	$('#why').click(function(){
 		if(click==0){
-            $('#whyTxt').text("Across our country many have had enough. They're"
+            $('#whyTxt').text("Across our country many teachers have had enough. They're"
             +" tired of working multiple jobs and want high salaries.")
             $('h4').css("color","#002868")
             $('p').css("color","#002868")
 			$('#why').css("padding","20px")
-            $('#why').css("text-align","left")
-            $('#why').hover
+			$('#why').css("text-align","left")
 			click++;
 		}
 		else{
